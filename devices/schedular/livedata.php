f205e05a361a9a6a0a2f8ffa414fd0577d66bc10
vidyaiq <110374328+vidyaiq@users.noreply.github.com>
2023-09-28 16:02:18 +0530

    date filters done on graph data displaying based on date filters

--- devices/schedular/livedata.php
+++ devices/schedular/livedata.php
@@ -1,47 +1,48 @@
 <?php
 header('Content-Type: application/json');
 include("../config.php");
+$chicagotime = date("Y-m-d H:i:s");
+$curdate = date('Y-m-d');
+$dateto = $curdate;
+$datefrom = $curdate;
+$button = "";
+$temp = "";
 
-$timestamp = date('H:i:s');
-$message = date("Y-m-d H:i:s");
-$chicagotime = date("d-m-Y");
+$_SESSION['date_from'] = "";
+$_SESSION['date_to'] = "";
+$_SESSION['button'] = "";
+$_SESSION['timezone'] = "";
 
+if (count($_POST) > 0) {
+    $_SESSION['date_from'] = $_POST['date_from'];
+    $_SESSION['date_to'] = $_POST['date_to'];
+    $_SESSION['button'] = $_POST['button'];
+    $_SESSION['timezone'] = $_POST['timezone'];
+    $dateto = $_POST['date_to'];
+    $datefrom = $_POST['date_from'];
+    $button = $_POST['button'];
+    $timezone = $_POST['timezone'];
+}
 $p_id = $_GET['p_id'];
-if(empty($dateto)){
-    $curdate = date('Y-m-d');
-    $dateto = $curdate;
-}
-
-if(empty($datefrom)){
-    $yesdate = date('Y-m-d',strtotime("-1 days"));
-    $datefrom = $yesdate;
-}
-
 //NEW VCODE
 if($p_id == 1){
-	$query = ("SELECT live_data.dev_id, live_data.temperature,DATE_FORMAT(live_data.datetime, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN iot_devices ON live_data.device_id =iot_devices.device_id INNER JOIN device_parameter_config on iot_devices.device_id = device_parameter_config.device_id where device_parameter_config.p_id = ".$p_id);
+	$query = ("SELECT live_data.dev_id, live_data.temperature,DATE_FORMAT(live_data.create_date, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN device_parameter_config on device_parameter_config.device_id = live_data.device_id where DATE_FORMAT(live_data.create_date, '%Y-%m-%d') BETWEEN '$datefrom' and '$dateto' and device_parameter_config.p_id = '1' ORDER BY `dev_id` DESC");
 }else if($p_id == 2){
-	$query = ("SELECT live_data.dev_id, live_data.humidity,DATE_FORMAT(live_data.datetime, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN iot_devices ON live_data.device_id =iot_devices.device_id INNER JOIN device_parameter_config on iot_devices.device_id = device_parameter_config.device_id where device_parameter_config.p_id = ".$p_id);
-	
+	$query = ("SELECT live_data.dev_id, live_data.humidity,DATE_FORMAT(live_data.create_date, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN device_parameter_config on device_parameter_config.device_id = live_data.device_id where DATE_FORMAT(live_data.create_date, '%Y-%m-%d') BETWEEN '$datefrom' and '$dateto' and device_parameter_config.p_id = '1' ORDER BY `dev_id` DESC");
 }else if($p_id == 3){
-    $query = ("SELECT live_data.dev_id, live_data.pressure,DATE_FORMAT(live_data.datetime, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN iot_devices ON live_data.device_id =iot_devices.device_id INNER JOIN device_parameter_config on iot_devices.device_id = device_parameter_config.device_id where device_parameter_config.p_id = ".$p_id);
-
+    $query = ("SELECT live_data.dev_id, live_data.pressure,DATE_FORMAT(live_data.create_date, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN device_parameter_config on device_parameter_config.device_id = live_data.device_id where DATE_FORMAT(live_data.create_date, '%Y-%m-%d') BETWEEN '$datefrom' and '$dateto' and device_parameter_config.p_id = '1' ORDER BY `dev_id` DESC");
 }
 else if($p_id == 4){
-    $query = ("SELECT live_data.dev_id, live_data.iaq,DATE_FORMAT(live_data.datetime, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN iot_devices ON live_data.device_id =iot_devices.device_id INNER JOIN device_parameter_config on iot_devices.device_id = device_parameter_config.device_id where device_parameter_config.p_id = ".$p_id);
-
+    $query = ("SELECT live_data.dev_id, live_data.iaq,DATE_FORMAT(live_data.create_date, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN device_parameter_config on device_parameter_config.device_id = live_data.device_id where DATE_FORMAT(live_data.create_date, '%Y-%m-%d') BETWEEN '$datefrom' and '$dateto' and device_parameter_config.p_id = '1' ORDER BY `dev_id` DESC");
 }else if($p_id == 5){
-    $query = ("SELECT live_data.dev_id, live_data.voc,DATE_FORMAT(live_data.datetime, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN iot_devices ON live_data.device_id =iot_devices.device_id INNER JOIN device_parameter_config on iot_devices.device_id = device_parameter_config.device_id where device_parameter_config.p_id = ".$p_id);
-
+    $query = ("SELECT live_data.dev_id, live_data.voc,DATE_FORMAT(live_data.create_date, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN device_parameter_config on device_parameter_config.device_id = live_data.device_id where DATE_FORMAT(live_data.create_date, '%Y-%m-%d') BETWEEN '$datefrom' and '$dateto' and device_parameter_config.p_id = '1' ORDER BY `dev_id` DESC");
 }
 else if($p_id == 6){
-    $query = ("SELECT live_data.dev_id, live_data.co2,DATE_FORMAT(live_data.datetime, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN iot_devices ON live_data.device_id =iot_devices.device_id INNER JOIN device_parameter_config on iot_devices.device_id = device_parameter_config.device_id where device_parameter_config.p_id = ".$p_id);
-
+    $query = ("SELECT live_data.dev_id, live_data.co2,DATE_FORMAT(live_data.create_date, '%d-%M-%Y %H:%i') as dTime,device_parameter_config.upper_tolerance,device_parameter_config.lower_tolerance FROM live_data INNER JOIN device_parameter_config on device_parameter_config.device_id = live_data.device_id where DATE_FORMAT(live_data.create_date, '%Y-%m-%d') BETWEEN '$datefrom' and '$dateto' and device_parameter_config.p_id = '1' ORDER BY `dev_id` DESC");
 }
 
 //execute query
 $result = mysqli_query($iot_db, $query);
-
 
 //loop through the returned data
 $data = array();
@@ -51,8 +52,6 @@ foreach ($result as $row) {
 
 //free memory associated with result
 $result->close();
-
-//close connection
 
 //now print the data
 print json_encode($data);
