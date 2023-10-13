4af021badcf1fc4f7b6faae417c3945f86fabfe4
nasrathf2001 <nasrath.f@iqhired.com>
2023-10-13 10:57:07 +0530

    switch button is_active

---
+++ devices/pages/devices/is_active_checked.php
@@ -0,0 +1,23 @@
+<?php
+include("../../config.php");
+$devc_check = $_POST['edit_is_active'];
+$check = $_POST['is_active'];
+$devc_is_checked = $_POST['isChecked'];
+if ($check != "" or $devc_check != "") {
+
+    if($devc_is_checked =='true') {
+        $sql1 = "UPDATE `iot_devices` set  is_active = 1  WHERE `device_id`='$check' or `device_id`='$devc_check'";
+    }else{
+        $sql1 = "UPDATE `iot_devices` set  is_active = 0  WHERE `device_id`='$check' or `device_id`='$devc_check'";
+    }
+    if (mysqli_query($iot_db, $sql1)) {
+        $_SESSION['message_stauts_class'] = 'alert-success';
+        $_SESSION['import_status_message'] = 'Required Sucessfully';
+    } else {
+        $_SESSION['message_stauts_class'] = 'alert-danger';
+        $_SESSION['import_status_message'] = 'please retry.';
+    }
+}
+
+header("Location:view_devices.php");
+?>
--- devices/pages/devices/view_devices.php
+++ devices/pages/devices/view_devices.php
@@ -50,6 +50,76 @@
     <link rel="stylesheet" href="<?php echo $iotURL; ?>assets/css/common.css"/>
     <!-- End layout styles -->
     <link rel="shortcut icon" href="<?php echo $iotURL ?>/assets/images/favicon.png"/>
+    <style>
+        /* The switch - the box around the slider */
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 43px;
+            height: 22px;
+        }
+
+        /* Hide default HTML checkbox */
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        /* The slider */
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        input:checked + .slider {
+            background-color: #28934a;
+        }
+
+        input:focus + .slider {
+            box-shadow: 0 0 1px #2196F3;
+        }
+
+        input:checked + .slider:before {
+            -webkit-transform: translateX(26px);
+            -ms-transform: translateX(26px);
+            transform: translateX(26px);
+        }
+
+        /* Rounded sliders */
+        .slider.round {
+            border-radius: 34px;
+        }
+
+        .slider.round:before {
+            border-radius: 50%;
+            width:18px;
+            height:18px;
+            margin-left:-11px;
+            margin-top:1px;
+            left: 4px;
+            bottom: 2px;
+        }
+        </style>
 
 </head>
 <body>
@@ -145,6 +215,7 @@
 
                                         while ($rowc = mysqli_fetch_array($qur)) {
                                         $t_id = $rowc["type_id"];
+                                        $is_active = $rowc['is_active'];
                                         $query1 ="SELECT `dev_type_name` FROM `iot_device_type` WHERE `type_id` = '$t_id'";
                                         $res = mysqli_query($iot_db,$query1);
                                         $rown = mysqli_fetch_array($res);
@@ -168,13 +239,14 @@
                                         <td><?php echo $rowc["device_location"]; ?></td>
 
                                         <td>
-											<?php
-												if ($rowc["is_active"] == 1) {
-													echo 'Yes';
-												} else {
-													echo 'No';
-												}
-											?>
+                                            <label class="switch">
+                                                <input type="checkbox" name="edit_is_active" id="edit_is_active" value="<?php echo $rowc["device_id"]; ?>" <?php echo ($rowc['is_active']==1 ? 'checked' : '');?>
+
+                                                <input type="checkbox">
+                                                <span class="slider round"></span>
+
+                                            </label>
+
                                         </td>
                                     </tr>
 									<?php } ?>
@@ -260,6 +332,22 @@
 <script src="<?php echo $iotURL ?>/assets/js/material.js"></script>
 <script src="<?php echo $iotURL ?>/assets/js/misc.js"></script>
 <!-- endinject -->
+<script>
+    $("input#edit_is_active").click(function () {
+        var isChecked = $(this)[0].checked;
+        var val = $(this).val();
+        var data_1 = "&edit_is_active=" + val+ "&isChecked=" + isChecked;
+        $.ajax({
+            type: 'POST',
+            url: "is_active_checked.php",
+            data: data_1,
+            success: function (response) {
+
+            }
+        });
+
+    });
+</script>
 <script>
     function deleteDevices(url) {
         $(':input[type="button"]').prop('disabled', true);
